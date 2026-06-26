8b6fc00d63ef35469151e6f9f50b013fc55442be
destinyseal <destinyseal.team@gmail.com>
2026-06-26 10:51:11 +0800

    Update cli.js

--- cli.js
+++ cli.js
@@ -2,9 +2,12 @@ import { drawOracle } from "./engine.js";
 
 const input = process.argv.slice(2).join(" ");
 
-const result = drawOracle(input);
+const res = drawOracle(input);
 
-console.log("\n🔮 臥龍一號結果\n");
-console.log("籤號:", result.index);
-console.log("籤文:", result.result.verse);
-console.log("解釋:", result.result.meaning);
+console.log("\n🔮 臥龍一號\n");
+console.log("籤號:", res.index);
+console.log("籤文:", res.result.verse);
+console.log("解釋:", res.result.meaning);
+
+console.log("\n📜 註釋：");
+res.note.forEach(n => console.log(" - " + n));
